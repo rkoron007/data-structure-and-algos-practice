a42aa98ef1fd940b4690fdecbbc1104fa8ccb529
Rose <rkoron007@gmail.com>
2020-03-23 10:21:21 -0700

    check perms

---
+++ leet_code_problems/strings/check-perms.js
@@ -0,0 +1,35 @@
+//
+
+const checkPerms = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+  let counter = hashCounter(str1);
+
+  for (let i = 0; i < str2.length; i++) {
+    const letter = str2[i];
+    if (counter[letter]) {
+      counter[letter] -= 1;
+    } else {
+      return false;
+    }
+  }
+  let values = Object.values(counter);
+  let aboveZeroValues = values.filter(num => num > 0);
+
+  return aboveZeroValues.length === 0;
+};
+
+const hashCounter = str => {
+  let hash = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (hash[letter]) {
+      hash[letter] += 1;
+    } else {
+      hash[letter] = 1;
+    }
+  }
+  return hash;
+};
+
+console.log(checkPerms("abcd", "dcbae"));
