964d3dc122b9b4e6cb8cf90094c1ecd0ce65209d
Rose <rkoron007@gmail.com>
2020-04-01 17:41:04 -0700

    balanced parens

--- ds_a_course_problems/stack_queue_interview_problems/lib/1_balanced_parens.js
+++ ds_a_course_problems/stack_queue_interview_problems/lib/1_balanced_parens.js
@@ -7,22 +7,22 @@
 // -------
 //
 // You are building a linter for your company's new custom text editor to keep
-// the code smells out of your (anticipated) massive codebase! Part of your 
-// technical design includes writing a function that checks that all of the 
+// the code smells out of your (anticipated) massive codebase! Part of your
+// technical design includes writing a function that checks that all of the
 // parentheses in your engineers' code are balanced.
 //
-// Given a string of text, write a function that returns true if the 
+// Given a string of text, write a function that returns true if the
 // parentheses are balanced, and false if they are not.
 //
-// Note: Your code should ignore all non-bracket characters in the input 
+// Note: Your code should ignore all non-bracket characters in the input
 //       string.
 //
 // ------
-// Bonus: 
+// Bonus:
 // ------
-//  
+//
 // Though you may want to start by writing a function that simply handles
-// parentheses as an MVP, to build a truly impactful product you must handle 
+// parentheses as an MVP, to build a truly impactful product you must handle
 // ALL bracket types, including:
 //
 //         - Parentheses:     ()
@@ -33,7 +33,7 @@
 //
 // ------------
 // Constraints:
-// ------------ 
+// ------------
 //
 // (1) Your function must run in linear time, O(n).
 // (2) Your function must consume (at maximum) linear space, O(n).
@@ -69,14 +69,94 @@
 // ------------------------------
 //
 // balancedParens('const roundDown = function(num) { return Math.floor(num) };');      => true
-// balancedParens('{ array: [1, 2, [3, 4], 5], timesTwoMethod: (num) => num * 2; }');  => true 
-// balancedParens('function printThirdElement(array) { console.log(array[3]]] }');     => false 
+// balancedParens('{ array: [1, 2, [3, 4], 5], timesTwoMethod: (num) => num * 2; }');  => true
+// balancedParens('function printThirdElement(array) { console.log(array[3]]] }');     => false
 //
 // -----------
 // Let's code!
 // -----------
-function balancedParens(str) {
 
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
+class Stack {
+  constructor() {
+    this.bottom = null;
+    this.top = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let node;
+    if (!this.top) {
+      node = new Node(value);
+      this.top = node;
+      this.bottom = node;
+    } else {
+      let oldTop = this.top;
+      node = new Node(value, oldTop);
+
+      this.top = node;
+    }
+
+    this.length += 1;
+    return this.length;
+  }
+
+  pop() {
+    if (this.length === 0) return null;
+    let oldTop = this.top;
+    if (this.length === 1) {
+      this.top = null;
+      this.bottom = null;
+    } else {
+      let newTop = oldTop.next;
+      this.top = newTop;
+    }
+
+    this.length -= 1;
+    if (this.length === 1) this.bottom = this.top;
+    return oldTop.value;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  empty() {
+    return this.size() === 0;
+  }
+
+  peek() {
+    return this.top.value;
+  }
+}
+
+function balancedParens(str) {
+  let stack = new Stack();
+  const pairs = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  };
+  // ()()
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (pairs[char]) {
+      stack.push(char);
+    } else if (char === "}" || char === "]" || char === ")") {
+      if (pairs[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(balancedParens("({)}"));
 exports.balancedParens = balancedParens;
