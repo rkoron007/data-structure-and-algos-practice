a658b34043fb38559aa7ad0f838ea336ccad54bb
Rose <rkoron007@gmail.com>
2020-04-03 16:00:07 -0700

    bst

--- ds_a_course_problems/depth_breadth_first_project/lib/tree_height.js
+++ ds_a_course_problems/depth_breadth_first_project/lib/tree_height.js
@@ -1,10 +1,21 @@
+const { TreeNode } = require("./tree_node");
 function treeHeight(root) {
-  let height = 0;
-  if (!root) return height;
-
-  return height;
+  if (!root) return -1;
+  //   if (!root.left || !root.right) return 0;
+  return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
 }
 
 module.exports = {
   treeHeight,
 };
+
+let a = new TreeNode("a");
+let b = new TreeNode("b");
+let c = new TreeNode("c");
+let d = new TreeNode("d");
+let e = new TreeNode("e");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+treeHeight(a);
