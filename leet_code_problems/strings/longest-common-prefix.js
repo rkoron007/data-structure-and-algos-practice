d5c5766e6b0740ef956b3a1a35f9683c3edcb445
Rose <rkoron007@gmail.com>
2020-04-20 10:24:09 -0700

    lcs

--- leet_code_problems/strings/longest-common-prefix.js
+++ leet_code_problems/strings/longest-common-prefix.js
@@ -1,51 +1,21 @@
-// Input: ["flower","flow","flight"]
-// Output: "fl"
-// const longestCommonPrefix = array => {
-//   if (array.length === 0) return "";
-//   if (array.length === 1) return array[0];
-//   let prefixes = createHash(array[0]);
-
-//   for (let i = 1; i < array.length; i++) {
-//     let word = array[i];
-//     for (let j = 0; j <= word.length; j++) {
-//       let prefix = word.slice(0, j);
-//       console.log(prefix);
-//       if (!prefixes[prefix]) {
-//         return word.slice(0, j - 1);
-//       }
-//     }
-//   }
-
-//   let commonPfix = Object.keys(prefixes);
-//   let longestPreFix = commonPfix.sort((a, b) => a.length - b.length)[
-//     commonPfix.length - 1
-//   ];
-//   return longestPreFix;
-// };
-
-// const createHash = word => {
-//   let letters = word.split("");
-//   let prefixHash = { "": true };
-//   for (let i = 0; i < letters.length; i++) {
-//     let prefix = letters.slice(0, i + 1).join("");
-//     prefixHash[prefix] = true;
-//   }
-
-//   return prefixHash;
-// };
-
-const longestCommonPrefix = array => {
-  if (array.length === 0) return "";
-  let prefix = array[0];
-  for (let i = 1; i < array.length; i++) {
-    while (array[i].indexOf(prefix) !== 0) {
-      console.log(prefix);
-      prefix = prefix.substring(0, prefix.length - 1);
-      console.log(prefix);
-      if (prefix === 0) return "";
+function longestCommonPrefix(array) {
+  let longestPrefix = "";
+  if (!array.length) return longestPrefix;
+  if (array.length < 2) return array[0];
+  let firstWord = array[0];
+  let lastWordIdx = array.length - 1;
+  for (let i = 0; i < firstWord.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      let word = array[j];
+      if (word[i] !== firstWord[i]) {
+        return longestPrefix;
+      } else if (j === lastWordIdx) {
+        longestPrefix += word[i];
+      }
     }
   }
-  return prefix;
-};
 
-console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+  return longestPrefix;
+}
+
+console.log(longestCommonPrefix(["a"]));
